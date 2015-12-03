c19f5f8e15350e5f2a9ccddf8cbaa8216d2c88ef
Konrad Fuger <konradfuger@googlemail.com>
2015-12-03 13:54:52 +0100

    initial commit

---
+++ DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/Slot.java
@@ -0,0 +1,30 @@
+package de.tuhh.diss.warehouse;
+
+import de.tuhh.diss.warehouse.sim.StoragePlace;
+
+public class Slot implements StoragePlace {	
+	
+	public int getNumber() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+
+	public int getWidth() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+
+	public int getHeight() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+	
+	public int getDepth() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+
+	public int getPositionX() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+	
+	public int getPositionY() {
+		return 0; // TODO: Replace this with your own code. 
+	}
+}
---
+++ DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/WarehouseApp.java
@@ -0,0 +1,8 @@
+package de.tuhh.diss.warehouse;
+
+public class WarehouseApp {
+	
+	public static void main (String[] args) {
+		
+	}
+}
