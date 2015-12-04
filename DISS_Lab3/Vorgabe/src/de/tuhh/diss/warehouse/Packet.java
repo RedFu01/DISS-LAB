700c411154359a9c417ea8302bfc8a8a5cdc0668
unknown <roedelknoedel>
2015-12-04 14:43:33 +0100

    interface and packet

--- DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/Packet.java
+++ DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/Packet.java
@@ -3,24 +3,63 @@ package de.tuhh.diss.warehouse;
 import de.tuhh.diss.warehouse.sim.StorageElement;
 
 public class Packet implements StorageElement{
+	private int width, height, depth, slot, id;
+	private String description;
 	
+	public Packet(int width, int height, int depth, String description){
+		this.width = width;
+		this.height = height;
+		this. depth = depth;
+		this.description = description;
+		//this. slot = slot;
+	}
+	
+	public Packet(){
+		this(0,0,0,"");
+	}
+	
+/*
+ * Function for Handover of the width of an element
+ * @see de.tuhh.diss.warehouse.sim.StorageElement#getWidth()
+ */
 	public int getWidth() {
-		return 0; // TODO: Replace this with your own code. 
-	}
-
-	public int getHeight() {
-		return 0; // TODO: Replace this with your own code. 
-	}
-
-	public int getDepth() {
-		return 0; // TODO: Replace this with your own code. 
-	}
-
-	public int getId() {
-		return 0; // TODO: Replace this with your own code. 
+		return width; 
 	}
 	
+/*
+ * Function for Handover of the height of an element
+ * @see de.tuhh.diss.warehouse.sim.StorageElement#getHeight()
+ */
+	public int getHeight() {
+		return height; 
+	}
+	
+/*
+ * Function for handover of the depth of an element
+ * @see de.tuhh.diss.warehouse.sim.StorageElement#getDepth()
+ */
+	public int getDepth() {
+		return depth; 
+	}
+
+/*
+ * Function for handover of the id of an element
+ * @see de.tuhh.diss.warehouse.sim.StorageElement#getId()
+ */
+	
+	public int getId() {
+		return id; 
+	}
+	
+/*
+ * Function for handover of the description of an element
+ * @see de.tuhh.diss.warehouse.sim.StorageElement#getDescription()
+ */	
 	public String getDescription() {
-		return null; // TODO: Replace this with your own code. 
+		return description; 
+	}
+	
+	public void setSlot(int newSlot){
+		slot = newSlot;
 	}
 }
--- DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/WarehouseApp.java
+++ DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/WarehouseApp.java
@@ -1,8 +1,89 @@
 package de.tuhh.diss.warehouse;
+import de.tuhh.diss.io.SimpleIO;
 
+import de.tuhh.diss.warehouse.test.*;
+import sun.awt.image.PNGImageDecoder.Chromaticities;
+import de.tuhh.diss.warehouse.sim.*;
 public class WarehouseApp {
 	
 	public static void main (String[] args) {
+		String choice = "";
+		double WIDTH = 0, HEIGHT = 0, DEPTH = 0;
+		String DESCRIPTION = "";
 		
+		// Display of the Header of the Menu 		
+		SimpleIO.println("Size: 16x5");
+		SimpleIO.println("PhysicalCrane simulator started, v1.0");
+		SimpleIO.println("");
+		SimpleIO.println("Welcome to TUHH/DISS Warehouse Managment");
+		SimpleIO.println("");
+		
+		
+
+		/*
+		 *  As long as the choice is not "0", the 
+		 */
+		do{ 
+			MainMenuText();
+			choiceString();
+			switch(choice){
+				case "1":
+					SimpleIO.println("*** Store a packet ***");
+					//Packet.width = readDouble();
+					DESCRIPTION = setDescription();
+					WIDTH = setWidth();
+					HEIGHT = setHeight();
+					DEPTH = setDepth();
+					Packet packet1 = new Packet();/*ID,DESCRIPTION, SLOTNUMBER, WIDTH, HEIGHT, DEPTH*/
+				case "2":
+					SimpleIO.println("*** Retrieve a packet ***");
+					
+					SimpleIO.println("Available packets: ");
+					SimpleIO.println("*** Enter ID of the packet to be retrieved (0 = abort)");
+				default:
+					SimpleIO.println("Please make a valid choice! ('1' or '2' or '0'");
+					
+			}
+		}while(choice != "0");
+		
+		SimpleIO.println("System ends.");
+		SimpleIO.println("PhysicalCrane was shut down.");
+	}
+	
+	public static String choiceString
+	(){
+		String choice = SimpleIO.readString();
+		SimpleIO.println("Your choice: "+choice);		
+		return(choice);
+	}
+	public String ReadString(){
+		return(SimpleIO.readString());
+	}
+	public double readDouble(){
+		return SimpleIO.readDouble();
+	}
+	
+	public static void MainMenuText(){
+		SimpleIO.println("*** Main Menu ***");
+		SimpleIO.println("1: Store a packet in the warehouse");
+		SimpleIO.println("2: Retrieve a packet from the warehouse");
+		SimpleIO.println("0: Quit programm");
+		SimpleIO.println("");
+	}
+	
+	/*
+	 * Methods for the input of width height and depth for seting up a new packet
+	 */
+	public static String setDescription(){
+		return(SimpleIO.readString());
+	}
+	public static double setWidth(){
+		return(SimpleIO.readDouble());
+	}
+	public static double setHeight(){
+		return(SimpleIO.readDouble());
+	}
+	public static double setDepth(){
+		return(SimpleIO.readDouble());
 	}
 }
