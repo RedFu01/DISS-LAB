2148e51b6c205b31ca5be76f584aedf3ffc27a06
Christopher <christopher.alexander1992@gmail.com>
2015-12-10 22:09:13 +0100

    Need to be tested.

--- DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/CraneControl.java
+++ DISS_Lab3/Vorgabe/src/de/tuhh/diss/warehouse/CraneControl.java
@@ -5,93 +5,113 @@ import de.tuhh.diss.warehouse.sim.PhysicalCrane;
 
 public class CraneControl {
 
-	private PhysicalCrane crane; // Define an instance variable for CraneControl class
+	// Define an instance variable crane as a type of PhysicalCrane
+	private PhysicalCrane crane;
 
 	public CraneControl(PhysicalCrane cr) {
-		
 		crane = cr;
 	}
 
+	// Drive the crane to the desired position of (x,y)
 	private void driveTo(int x, int y) {
 		driveToX(x);
 		driveToY(y);
 	}
 
+	// Drive the crane to the desired position of (x)
 	private void driveToX(int x) {
-		int currentX = crane.getPositionX();
+		int initialX = crane.getPositionX();
 		int targetX = x;
-		
-		if (currentX < targetX) {
-			while (currentX < targetX) {
+
+		if (initialX < targetX) {
+			while (initialX < targetX) {
 				crane.forward();
 				stallTestX();
 			}
 		}
-		
-		if (currentX > targetX) {
-			while (currentX > targetX) {
+
+		if (initialX > targetX) {
+			while (initialX > targetX) {
 				crane.backward();
 				stallTestX();
 			}
 		}
-		
+
 		else {
 			crane.stopX();
 		}
 	}
 
+	// Drive the crane to the desired position of (y)
 	private void driveToY(int y) {
-		int currentY = crane.getPositionY();
+		int initialY = crane.getPositionY();
 		int targetY = y;
-		
-		if (currentY < targetY) {
-			while (currentY < targetY) {
+
+		if (initialY < targetY) {
+			while (initialY < targetY) {
 				crane.up();
 				stallTestY();
 			}
 		}
-		
-		if (currentY > targetY) {
-			while (currentY > targetY) {
+
+		if (initialY > targetY) {
+			while (initialY > targetY) {
 				crane.down();
 				stallTestY();
 			}
 		}
-		
+
 		else {
 			crane.stopY();
 		}
 	}
 
+	// Drive the crane to the loading/unloading bay
 	private void driveToLoadingPosition() {
 		int LoadPosX = crane.getLoadingPosX();
 		int LoadPosY = crane.getLoadingPosY();
-		
-		driveTo(LoadPosX,LoadPosY);
+
+		driveTo(LoadPosX, LoadPosY);
 	}
 
-	private void stallTestX() {		
+	private void stallTestX() {
 		// Exception
+		if (crane.isStalledX() == true) {
+			throw new IllegalArgumentException("Crane stalled at x = " + crane.getPositionX());
+		}
 	}
 
 	private void stallTestY() {
 		// Exception
+		if (crane.isStalledY() == true) {
+			throw new IllegalArgumentException("Crane stalled at y = " + crane.getPositionY());
+		}
 	}
 
 	public void storePacket(int x, int y, StorageElement packet) {
-		
+		if (packet == null) {
+			throw new IllegalArgumentException(
+					"CraneControl.storePacket - there is no packet in the crane at the moment.");
+		}
+
+		// Drive to the loading/unloading bay, then load packet into the crane
 		driveToLoadingPosition();
-		
-		
+		crane.loadElement(packet);
+
+		// Drive to the storage space, then store the packet
+		driveTo(x, y);
 		crane.storeElement();
+
 	}
 
 	public StorageElement retrievePacket(int x, int y) {
-		
-		
-		
+		// Drive to the storage space, then load the packet into the crane
+		driveTo(x, y);
 		crane.retrieveElement();
-		return null;
+
+		// Drive to the loading/unloading bay, then unload packet off the crane
+		driveToLoadingPosition();
+		return crane.unloadElement();
 	}
 
 	public void shutdown() {
